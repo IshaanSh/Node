305d79507120d3feef463767796c3428a0ee9cfd
Appinventiv <manishasharma20009999160805@gmail.com>
2023-07-05 12:43:59 +0530

    updation

--- practice.ts
+++ practice.ts
@@ -1,24 +1,62 @@
-const express= require('express')
-const bodyParser = require('body-parser');
+// const http = require('http');
 
 
-const app = express();
-const port = 3000;
+// const server = http.createServer((req,res)=>{
+//     res.write("hello world")
+//     res.end()
+// })
 
-app.use(bodyParser());
-
-
-app.get('/add',(req, res)=>{
-  let sum = req.body.num1 + req.body.num2;
-  // console.log(req.body.sum1);
-  res.send(`sum is ${sum}`)
-})
-
-app.get('/mul',(req, res)=>{
-    let sum = req.body.num1 * req.body.num2;
-    // console.log(req.body.sum1);
-    res.send(`mul is ${sum}`)
-  })
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+// server.listen(3000 , ()=>{
+    //     console.log(`listen on ${port}`);
+    // })
+    
+    import { Response,Request } from "express"
+    const express = require('express')
+    const app = express()
+    
+    const bodyParser = require('body-parser')
+    
+    const port = 3000 
+    
+    app.use(bodyParser.json())
+    
+    app.post('/sum',(req:Request,res:Response)=>{
+        let sum = req.body.num1 + req.body.num2
+        res.send(`Sum of ${req.body.num1} and ${req.body.num2} is ${sum}`)
+    })
+    
+    app.get('/subtraction',(req:Request,res:Response)=>{
+        let sub = req.body.num1 - req.body.num2
+        res.send(`Subtraction of ${req.body.num1} and ${req.body.num2} is ${sub}`)
+    })
+    
+    app.get('/multiply',(req:Request,res:Response)=>{
+        let product = req.body.num1 * req.body.num2
+        res.send(`Product of ${req.body.num1} and ${req.body.num2} is ${product}`)
+    })
+    
+    app.get('/divide',(req:Request,res:Response)=>{
+        let divide = req.body.num2 / req.body.num1
+        res.send(`Divison of ${req.body.num1} and ${req.body.num2} is ${divide}`)
+    })
+    
+    app.get('/modulus',(req:Request,res:Response)=>{
+        let modulo = req.body.num2 % req.body.num1
+        res.send(`Modulus of ${req.body.num1} and ${req.body.num2} is ${modulo}`)
+    })
+    
+    app.get('/square-root',(req:Request,res:Response)=>{
+        let square = req.body.num1 ** req.body.num2
+        res.send(`Square Root of ${req.body.num1} and ${req.body.num2} is ${square}`)
+    })
+    
+    app.get('/log',(req:Request,res:Response)=>{
+        let log = Math.log(req.body.num1)
+        res.send(`Log of ${req.body.num1} is ${log}`)
+    })
+    
+    
+    app.listen(port,()=>{
+        console.log(`listining on hhh ${port}`);
+    })
+    
